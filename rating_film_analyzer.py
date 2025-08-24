dde0389a406ba8baeef2da0abc21e19e12f8045e
Radik <radik.amirov.63@inbox.ru>
2025-08-24 16:43:51 +0500

    refactor(rating): Move data access logic to a separate function

--- rating_film_analyzer.py
+++ rating_film_analyzer.py
@@ -5,73 +5,10 @@ import argparse
 import csv
 from database import execute_query
 
-# -------------------------------------------
-# БЛОК 1: ФУНКЦИЯ ДЛЯ ОТОБРАЖЕНИЯ ДАННЫХ
-# -------------------------------------------
-def display_results(films):
-    if not films:
-        print("Нет данных для отображения.")
-        return
-    print("\n--- Топ самых длинных фильмов по рейтингам ---")
-    current_rating = None #
-
-    for title, rating, length in films:
-        if current_rating != rating:
-            if current_rating is not None:
-                print('-' * 55)
-            current_rating = rating
-            print(f"\nРейтинг: {current_rating}")
-            print('-' * 55)
-            print(f"| {'Название фильма':<35} | "
-                  f"{'Длительность':>13} |")
-            print('-' * 55)
-
-        print(f"| {title:<35} | {length:>9} мин |")
-
-    if films:
-        print('-' * 55)
-
-# -------------------------------------------
-# БЛОК 2: ФУНКЦИЯ ДЛЯ СОХРАНЕНИЯ В CSV-файл
-# -------------------------------------------
-def save_report_to_csv(data, filename):
-    if not data:
-        print("Нет данных для сохранения в файл.")
-        return
-    header = ['Название фильма', 'Рейтинг', 'Длительность (мин)']
-    print(f"\nСохраняю отчет в файл {filename}...")
-
-    try:
-        with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
-            writer = csv.writer(csvfile) # Создаем спец. объект — "писатель"
-            writer.writerow(header)
-            writer.writerows(data)
-        print(f"Отчет успешно сохранен в файл {filename}")
-
-    except IOError as e:
-        print(f"Ошибка при сохранении файла: {e}")
-
-# -------------------------------------------
-# БЛОК 3: ГЛАВНАЯ ФУНКЦИЯ, УПРАВЛЯЮЩАЯ ЛОГИКОЙ СКРИПТА
-# -------------------------------------------
-def main():
-    parser = argparse.ArgumentParser(
-            description="Создает отчет по самым длинным "
-                        "фильмам для каждого рейтинга."
-    )
-    parser.add_argument(
-            '--limit',
-            type=int,
-            required=True,
-            help="Обязательный параметр. Ранг, до которого "
-                 "включать фильмы в отчет (например, 3 для Топ-3)."
-    )
-    parser.add_argument(
-            '--output',
-            default="top_films_long_rating.csv",
-            help="Имя файла для сохранения CSV-отчета."
-    )
-    args = parser.parse_args()
+#-------------------------------------------
+# БЛОК 1: ФУНКЦИЯ ДЛЯ ПОЛУЧЕНИЯ ДАННЫХ ИЗ БД
+#-------------------------------------------
+def get_top_rating_film_data(limit):
 
     # Формируем запрос и вызываем универсальную функцию
     sql_query = """
@@ -103,10 +40,80 @@ def main():
             length desc -- Вторичная сортировка по длине для красоты вывода            
     """
 
-    film_data = execute_query(sql_query,(args.limit,))
+    film_data = execute_query(sql_query,(limit,))
+    return film_data
 
+# -------------------------------------------
+# БЛОК 2: ФУНКЦИЯ ДЛЯ ОТОБРАЖЕНИЯ ДАННЫХ
+# -------------------------------------------
+def display_results(films, limit):
+    if not films:
+        print("Нет данных для отображения.")
+        return
+    print(f"\n--- Топ-{limit} самых длинных фильмов по рейтингам ---")
+    current_rating = None #
+
+    for title, rating, length in films:
+        if current_rating != rating:
+            if current_rating is not None:
+                print('-' * 55)
+            current_rating = rating
+            print(f"\nРейтинг: {current_rating}")
+            print('-' * 55)
+            print(f"| {'Название фильма':<35} | "
+                  f"{'Длительность':>13} |")
+            print('-' * 55)
+
+        print(f"| {title:<35} | {length:>9} мин |")
+
+    if films:
+        print('-' * 55)
+
+# -------------------------------------------
+# БЛОК 3: ФУНКЦИЯ ДЛЯ СОХРАНЕНИЯ В CSV-файл
+# -------------------------------------------
+def save_report_to_csv(data, filename):
+    if not data:
+        print("Нет данных для сохранения в файл.")
+        return
+    header = ['Название фильма', 'Рейтинг', 'Длительность (мин)']
+    print(f"\nСохраняю отчет в файл {filename}...")
+
+    try:
+        with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.writer(csvfile) # Создаем спец. объект — "писатель"
+            writer.writerow(header)
+            writer.writerows(data)
+        print(f"Отчет успешно сохранен в файл {filename}")
+
+    except IOError as e:
+        print(f"Ошибка при сохранении файла: {e}")
+
+# -------------------------------------------
+# БЛОК 4: ГЛАВНАЯ ФУНКЦИЯ, УПРАВЛЯЮЩАЯ ЛОГИКОЙ СКРИПТА
+# -------------------------------------------
+def main():
+    parser = argparse.ArgumentParser(
+            description="Создает отчет по самым длинным "
+                        "фильмам для каждого рейтинга."
+    )
+    parser.add_argument(
+            '--limit',
+            type=int,
+            required=True,
+            help="Обязательный параметр. Ранг, до которого "
+                 "включать фильмы в отчет (например, 3 для Топ-3)."
+    )
+    parser.add_argument(
+            '--output',
+            default="top_films_long_rating.csv",
+            help="Имя файла для сохранения CSV-отчета."
+    )
+    args = parser.parse_args()
+
+    film_data = get_top_rating_film_data(args.limit)
     if film_data:
-        display_results(film_data)
+        display_results(film_data, args.limit)
         save_report_to_csv(film_data, args.output)
     else:
         print("Не удалось получить данные. Программа завершена.")
